4735068a62c311275cd7c49702347af7d97616b1
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-11-07 11:19:07 +0100

    récupération du code de sgmap/mes-aides-ui

---
+++ app/models/Situation.java
@@ -0,0 +1,14 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Situation {
+
+    public List<Individu> individus = new ArrayList<>();
+    public Logement logement;
+    public Integer immobilierValue;
+    public Integer mobilierValue;
+    public String email;
+    public String phoneNumber;
+}
